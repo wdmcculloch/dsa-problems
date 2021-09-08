afc84ceb416f6adc50e7b85b839268795299d384
Will McCulloch <willdmcculloch@gmail.com>
2021-09-08 12:35:00 -0700

    implement longestPalindrome --> brute force method

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -1,9 +1,37 @@
 /**
  * @param {string} s
  * @return {string}
+ * @edgeCase if multiple palindromes of the same length return first
+ * @edgeCase if length === 1 return entire string
+ * @constraints 1 <= s.length <= 1000
+ * @constraints s consists of only english letters
  */
- var longestPalindrome = function(s) {
+var longestPalindrome = function (s) {
+  if (s.length === 1) {
+    return s;
+  }
+  let longestPalindrome = "";
+  for (let i = 0; i < s.length; i++) {
+    for (let j = 0; j < s.length; j++) {
+      let current = s.slice(i, j + 1);
+      if (isPalindrome(current) && current.length > longestPalindrome.length) {
+        longestPalindrome = current;
+      }
+    }
+  }
+  return longestPalindrome;
+};
 
+var reverseString = (s) => {
+  let newStr = "";
+  for (let i = s.length - 1; i >= 0; i--) {
+    newStr += s[i];
+  }
+  return newStr;
+};
+
+var isPalindrome = (s) => {
+  return s === reverseString(s);
 };
 
 module.exports = longestPalindrome;
--- longestPalindrome/longestPalindrome.test.js
+++ longestPalindrome/longestPalindrome.test.js
@@ -1,7 +1,17 @@
-const longestPalindrome = require('./longestPalindrome.js');
+const longestPalindrome = require("./longestPalindrome.js");
 
-describe('', () => {
-  test('', () => {
+describe("longestPalindrome", () => {
+  test("returns the first longestPalindrome", () => {
+    expect(longestPalindrome("babad")).toBe("bab");
+    expect(longestPalindrome("cbbd")).toBe("bb");
+    expect(longestPalindrome("ac")).toBe("a");
+  });
 
-  })
-})
+  test("handles string with length of one", () => {
+    expect(longestPalindrome("a")).toBe("a");
+  });
+
+  test("handles empty string", () => {
+    expect(longestPalindrome("")).toBe("");
+  });
+});
