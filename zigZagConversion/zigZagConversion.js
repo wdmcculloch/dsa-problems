b481f28fcfde83500f34f37352af76ef2ed0996a
Will McCulloch <willdmcculloch@gmail.com>
2021-09-13 11:10:52 -0700

    complete zigZag conversion

--- zigZagConversion/zigZagConversion.js
+++ zigZagConversion/zigZagConversion.js
@@ -3,8 +3,37 @@
  * @param {number} numRows
  * @return {string}
  */
- var convert = function(s, numRows) {
+var convert = function (s, numRows) {
+  if (numRows === 1) {
+    return s;
+  }
 
+  let matrix = [];
+  //build matrix
+  for (let i = 0; i < numRows; i++) {
+    matrix.push("");
+  }
+
+  let row = 0;
+  let upDown = -1;
+
+  //iterate over string - fill rows
+  for (let j = 0; j < s.length; j++) {
+    let char = s[j];
+    matrix[row] += char;
+    if (row === 0 || row === numRows - 1) {
+      upDown *= -1;
+    }
+    row += upDown;
+  }
+
+  let output = "";
+  //build rows into new string
+  for (let k = 0; k < matrix.length; k++) {
+    output += matrix[k];
+  }
+
+  return output;
 };
 
 module.exports = convert;
--- zigZagConversion/zigZagConversion.test.js
+++ zigZagConversion/zigZagConversion.test.js
@@ -1,7 +1,12 @@
 const convert = require('./zigZagConversion.js');
 
 describe('zig zag conversion', () => {
-  test('', () => {
-
+  test('returns the correct string', () => {
+    expect(convert('PAYPALISHIRING', 3)).toBe('PAHNAPLSIIGYIR');
+    expect(convert('PAYPALISHIRING', 4)).toBe('PINALSIGYAHRPI');
+  })
+  test('returns correct string for row length of 1', () => {
+    expect(convert('A', 1)).toBe('A');
+    expect(convert('AB', 1)).toBe('AB');
   })
 })
